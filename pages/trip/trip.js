f6fe968162a435d1c1f0e26cc43d5751c108fec2
Romoo <romoochen+git@gmail.com>
2016-11-03 17:10:06 +0800

    add trip page

---
+++ pages/trip/trip.js
@@ -0,0 +1,51 @@
+const api = require('../../utils/api.js');
+
+const app = getApp();
+Page({
+  data: {
+    trip: null,
+    options: null,
+    windowWidth: 0,
+  },
+  onReady() {
+    const self = this;
+    wx.setNavigationBarTitle({
+      title: self.data.options.name,
+    });
+  },
+  onLoad(options) {
+    const self = this;
+    const id = options.id;
+    self.setData({
+      options,
+      windowWidth: app.systemInfo.windowWidth,
+    });
+    wx.showToast({
+      title: '正在加载',
+      icon: 'loading',
+      duration: 10000,
+    });
+    api.trip.waypoints(id, (state, res) => {
+      if (state === 'success') {
+        const trip = res.data;
+        self.setData({
+          trip,
+        });
+        wx.hideToast();
+      }
+    });
+  },
+  viewWaypoint(e) {
+    const self = this;
+    const ds = e.currentTarget.dataset;
+    wx.navigateTo({
+      url: `../waypoint/waypoint?waypointId=${ds.waypoint}&tripId=${self.data.trip.id}`,
+    });
+  },
+  gotoUser(e) {
+    const userId = e.target.dataset.id;
+    wx.navigateTo({
+      url: `../user/user?id=${userId}`,
+    });
+  },
+});
